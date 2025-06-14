7c2f947884189d5c355c99c4e9d674675e063cbd
okkyok <takumichi.aust@gmail.com>
2025-06-15 08:50:35 +0900

    try_修正1

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -40,7 +40,7 @@ export default (() => {
       <head>
         <title>{title}</title>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover" />
         {cfg.theme.cdnCaching && cfg.theme.fontOrigin === "googleFonts" && (
           <>
             <link rel="preconnect" href="https://fonts.googleapis.com" />
