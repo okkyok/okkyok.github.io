cdc3be752617de0bb87efcc1dea02be5520a66af
okkyok <takumichi.aust@gmail.com>
2025-06-15 08:44:29 +0900

    try_ローカル環境をwebに反映

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -40,6 +40,7 @@ export default (() => {
       <head>
         <title>{title}</title>
         <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         {cfg.theme.cdnCaching && cfg.theme.fontOrigin === "googleFonts" && (
           <>
             <link rel="preconnect" href="https://fonts.googleapis.com" />
