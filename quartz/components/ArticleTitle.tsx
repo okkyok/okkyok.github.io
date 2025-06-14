7a5515ad11c8c0e5ede183c5ad76843161f8355f
okkyok <takumichi.aust@gmail.com>
2025-06-14 10:23:25 +0900

    動作ok_h1.article-titleのサイズ変更

--- quartz/components/ArticleTitle.tsx
+++ quartz/components/ArticleTitle.tsx
@@ -13,6 +13,8 @@ const ArticleTitle: QuartzComponent = ({ fileData, displayClass }: QuartzCompone
 ArticleTitle.css = `
 .article-title {
   margin: 2rem 0 0 0;
+  font-size: 2.0rem;
+  line-height: 2.4rem;
 }
 `
 
