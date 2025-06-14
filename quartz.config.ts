3a74228de3e6360939e612794f61c52b0cdf56c7
okkyok <takumichi.aust@gmail.com>
2025-06-14 14:35:08 +0900

    動作ok_虫眼鏡、ライトモードダークモードのボタンの色変更

--- quartz.config.ts
+++ quartz.config.ts
@@ -35,9 +35,9 @@ const config: QuartzConfig = {
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
           dark: "#2b2b2b",
-          secondary: "#284b63",
-          tertiary: "#84a59d",
-          highlight: "rgba(143, 159, 169, 0.15)",
+          secondary: "#627883", // 虫眼鏡やUI要素の色
+          tertiary: "#627883", // ホバー時の色など
+          highlight: "rgba(98, 120, 131, 0.15)",
           textHighlight: "#fff23688",
         },
         darkMode: {
@@ -46,9 +46,9 @@ const config: QuartzConfig = {
           gray: "#646464",
           darkgray: "#d4d4d4",
           dark: "#ebebec",
-          secondary: "#7b97aa",
-          tertiary: "#84a59d",
-          highlight: "rgba(143, 159, 169, 0.15)",
+          secondary: "#627883", // 虫眼鏡やUI要素の色
+          tertiary: "#627883", // ホバー時の色など
+          highlight: "rgba(98, 120, 131, 0.15)",
           textHighlight: "#b3aa0288",
         },
       },
