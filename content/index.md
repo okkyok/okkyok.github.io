3eedaba30acaa809fa682ab7eb4f787827dd0e36
okkyok <takumichi.aust@gmail.com>
2025-07-12 16:05:26 +0900

    トップページのタイトル非表示

--- content/index.md
+++ content/index.md
@@ -2,3 +2,13 @@
 title: index
 ---
 
+![[loventia_logo_good.webp]]
+
+LOVENTIA
+
+
+
+![[!プロフィール画像.jpeg]]
+
+[[👤okky]]が運営するブログ。
+サイト名のLOVENTIAは、愛と冒険をもって世界を楽しむことにちなんで。
--- quartz/components/ArticleTitle.tsx
+++ quartz/components/ArticleTitle.tsx
@@ -3,7 +3,7 @@ import { classNames } from "../util/lang"
 
 const ArticleTitle: QuartzComponent = ({ fileData, displayClass }: QuartzComponentProps) => {
   const title = fileData.frontmatter?.title
-  if (title) {
+  if (title && fileData.slug !== "index") {
     return <h1 class={classNames(displayClass, "article-title")}>{title}</h1>
   } else {
     return null
